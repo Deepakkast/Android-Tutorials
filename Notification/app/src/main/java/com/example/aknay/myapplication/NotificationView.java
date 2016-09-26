14bf047b05639e60245b7906153183dc121931fe
aknay <aknay@debian>
2016-09-26 22:57:13 +0800

    added notification example

---
+++ Notification/app/src/main/java/com/example/aknay/myapplication/NotificationView.java
@@ -0,0 +1,16 @@
+package com.example.aknay.myapplication;
+
+/**
+ * Created by aknay on 23/9/16.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
